65c90fe2f4a8780f324552259b3e257211e5d2bc
Courtney Ives <cgives@me.com>
2016-04-04 00:27:39 -0500

    add some styles, clear buttons

--- app.js
+++ app.js
@@ -1,27 +1,39 @@
 
 $(function (){
 
-// ----------- buttons ----------- //
-  $('.movies-area button').click(function() {
-    var movieTitle = $(this).parent().children('input').first().serialize();
+  // ----------- buttons ----------- //
+  $('.movies-area button.add-button').click(function() {
+    var area = $(this).parent();
+    var movieTitle = area.children('input').first().serialize();
     var request = getMovie(movieTitle);
+    $(this).prop('disabled', true);
+    $(this).parent().children('button.clear-button').prop('disabled', false);
     request.done(function(response){
-      showMovie(response);
+      showMovie(response, area);
+
     });
   });
 
-  function getMovie(movieTitle) {
-    return $.get('http://www.omdbapi.com/?' + movieTitle);
+  $('.movies-area button.clear-button').click(function() {
+    $(this).prop('disabled', true);
+    $(this).parent().children('ul').empty();
+    $(this).parent().children('img').remove();
+    $(this).parent().children('h1').empty();
+    $(this).parent().children('button.add-button').prop('disabled', false);
+  });
 
-  }
 
-  function showMovie(response) {
-    console.log(response);
-    console.log(response.Title);
-    console.log(response.Year);
-    console.log(response.Poster);
-    console.log(response.Actors);
-    console.log(response.Plot);
-  }
+    function getMovie(movieTitle) {
+      return $.get('http://www.omdbapi.com/?' + movieTitle);
 
-});
+    }
+
+    function showMovie(response, area) {
+      area.children('h1').append(response.Title);
+      area.children('ul').append('<li><h2>' + response.Year + '</h2></li>');
+      area.children('ul').append('<li>' + response.Actors + '</li>');
+      area.children('ul').append('<li>' + response.Plot + '</li>');
+      area.append('<img src="' + response.Poster + '" alt="Movie Poster" />');
+    }
+
+  });
